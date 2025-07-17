cc7f4053f00466950fc9e01ce5520f495862c026
NhatNhu <nhudinhnhat2004@gmail.com>
2025-07-17 13:35:20 +0700

    ok

--- src/main/java/com/rental/houserental/controller/BookingController.java
+++ src/main/java/com/rental/houserental/controller/BookingController.java
@@ -8,6 +8,7 @@ import com.rental.houserental.service.PropertyService;
 import com.rental.houserental.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
